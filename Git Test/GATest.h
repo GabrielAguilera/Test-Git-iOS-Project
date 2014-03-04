b21de9da82f7545ceef17316973acf8050b4dd77
Gabriel Aguilera <gabrieag@umich.edu>
2014-03-04 03:55:26 -0500

    added a test class

---
+++ Git Test/GATest.h
@@ -0,0 +1,13 @@
+//
+//  GATest.h
+//  Git Test
+//
+//  Created by Gabriel Aguilera on 3/4/14.
+//  Copyright (c) 2014 Gabriel Aguilera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GATest : NSObject
+
+@end
---
+++ Git Test/GATest.m
@@ -0,0 +1,13 @@
+//
+//  GATest.m
+//  Git Test
+//
+//  Created by Gabriel Aguilera on 3/4/14.
+//  Copyright (c) 2014 Gabriel Aguilera. All rights reserved.
+//
+
+#import "GATest.h"
+
+@implementation GATest
+
+@end
